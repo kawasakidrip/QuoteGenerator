f238cd04b27e895343f74720f7949255e6d09165
kawasakidrip <leinchmo@gmail.com>
2023-06-21 23:23:55 +0300

    upd

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,22 @@
-import axios from 'axios';
-
 async function getQuote() {
   try {
-    const response = await axios.request({
+    const response = await fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=ru', {
       method: 'GET',
-      url: 'https://quotes15.p.rapidapi.com/quotes/random/',
-      params: {
-        language_code: 'ru',
-      },
       headers: {
         'X-RapidAPI-Key': '81d549cfaemsh060cff6e4b4677ep1c0aeajsn1ee45e409c7a',
         'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
       },
     });
 
-    const quote = response.data.content;
-    const author = response.data.originator.name;
-
-    const quoteContainer = document.getElementById('content-wrapper');
-    quoteContainer.innerHTML = `"${quote}" - ${author}`;
+    if (response.ok) {
+      const data = await response.json();
+      const quote = data.content;
+      const author = data.originator.name;
+      const quoteContainer = document.getElementById('content-wrapper1');
+      quoteContainer.innerHTML = `"${quote}" - ${author}`;
+    } else {
+      throw new Error('Ошибка при получении цитаты');
+    }
   } catch (error) {
     console.error('Ошибка при получении цитаты:', error.message);
   }
@@ -27,9 +25,8 @@ async function getQuote() {
 getQuote();
 
 document.addEventListener('DOMContentLoaded', function() {
-  const generateButton = document.getElementById('generate-btn');
-
+  const generateButton = document.getElementById('gen-btn');
   generateButton.addEventListener('click', function() {
     getQuote();
-  });
+ });
 });
