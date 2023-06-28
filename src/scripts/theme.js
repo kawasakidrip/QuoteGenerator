a35940ad8869fb36d86f2edb9b46a02ffe351da4
kawasakidrip <leinchmo@gmail.com>
2023-06-28 04:51:36 +0300

    add tumbler

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,13 +1,11 @@
-function switchTheme() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    
-    var darkTheme = document.getElementById("dark-theme");
-    if (element.classList.contains("dark-mode")) {
+function toggleTheme() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 
-      darkTheme.disabled = false;
-    } else {
-      darkTheme.disabled = true;
-    }
+  var darkTheme = document.getElementById("dark-theme");
+  if (element.classList.contains("dark-mode")) {
+    darkTheme.disabled = false;
+  } else {
+    darkTheme.disabled = true;
   }
-  
+}
